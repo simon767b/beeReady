a0f1047e7d92f8ce2973571c8b3dde73c6f8e33e
Inger-Margrethe <imhp@hotmail.dk>
2024-10-16 09:51:51 +0200

    start comp List

--- src/components/list.jsx
+++ src/components/list.jsx
@@ -2,7 +2,9 @@ export default function List() {
   return (
     <section>
       <h6>Riga</h6>
-      
+      <p>Maj 2024</p>
+      <img src="" alt="" />
+      <p>17/20</p>
     </section>
   );
 }
