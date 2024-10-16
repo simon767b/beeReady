ae920f3d383acc1a7a82b45dea1afd55bae6397b
simon767b <simon767b@gmail.com>
2024-10-16 10:10:32 +0200

    ikoner

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -2,15 +2,15 @@ export default function Nav() {
   return (
     <div className="navbar">
       <div className="navbar_navitems">
-        <img src="./img/icons/bruger.svg" alt="" />
+        <img src="./img/icons/bruger.svg" alt="nav ikon" />
         <p>Bruger</p>
       </div>
       <div className="navbar_navitems">
-        <img src="" alt="" />
+        <img src="./img/icons/lister.svg" alt="nav ikon" />
         <p>Bruger</p>
       </div>
       <div className="navbar_navitems">
-        <img src="" alt="" />
+        <img src="./img/icons/indstillinger.svg" alt="nav ikon" />
         <p>Bruger</p>
       </div>
     </div>
