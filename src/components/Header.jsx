e5e8fadd56ef707e76f21023047fdb5d97bf96a6
andreaskadhede <andreaskadhede@icloud.com>
2024-10-16 11:26:34 +0200

    header done

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,9 +3,9 @@ import { Link } from "react-router-dom";
 export default function Header() {
    return (
       <>
-         <Link>
-            <img src="img/logo/logo.svg" alt="" />
-            <img src="img/logo/logo-tekst-lang.svg" alt="" />
+         <Link to="/" className="header">
+            <img className="logo-img" src="img/logo/logo.svg" alt="" />
+            <img className="logo-text" src="img/logo/logo-tekst-lang.svg" alt="" />
          </Link>
       </>
    );
