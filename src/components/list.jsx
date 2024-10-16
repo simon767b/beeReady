e54ddb36adfbf6e77f61e664a5f57123eaba2502
Inger-Margrethe <imhp@hotmail.dk>
2024-10-16 11:26:40 +0200

    list comp work

--- src/components/list.jsx
+++ src/components/list.jsx
@@ -1,10 +1,21 @@
 export default function List() {
+  function calcItemCount() {
+    const itemsChecked = 17;
+    const itemsTotal = 20;
+    const hexHeight = 125;
+
+    const itemsDone = (hexHeight / itemsTotal) * itemsChecked;
+    return itemsDone;
+  }
+  const itemsDone = calcItemCount();
+
   return (
-    <section>
+    <section className="beehive">
       <h6>Riga</h6>
-      <p>Maj 2024</p>
+      <p className="date">Maj 2024</p>
       <img src="\img\icons\tourist.svg" alt="Turist ikon" />
-      <p>17/20</p>
+      <p className="item_count">17/20</p>
+      <div className="wave" style={{ height: itemsDone }}></div>
     </section>
   );
 }
