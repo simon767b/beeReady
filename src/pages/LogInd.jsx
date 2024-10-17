39984b8602c5554332c746434a2dcb36f1d69606
simon767b <simon767b@gmail.com>
2024-10-17 13:49:38 +0200

    Log ind

--- src/pages/LogInd.jsx
+++ src/pages/LogInd.jsx
@@ -1,7 +1,29 @@
 export default function LogInd() {
   return (
     <>
-      <img src="./img/bi-med-taske.gif" alt="flyvende bi" className="biGIF" />
+      <div>
+        <img src="./img/bi-med-taske.gif" alt="flyvende bi" className="biGIF" />
+        <img
+          src="./img/logo/logo-tekst-kort.svg"
+          alt="lgog tekst"
+          className="biGIF"
+        />
+
+        <form>
+          <p>Log ind</p>
+          <div>
+            <p>Email:</p>
+            <input type="text" placeholder="Patricia77@gmail.com" />
+          </div>
+
+          <div>
+            <p>Kode:</p>
+            <input type="text" placeholder="***********" />
+          </div>
+        </form>
+
+        <button className="generelKnap">Log ind</button>
+      </div>
     </>
   );
 }
