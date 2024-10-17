34ce0c1b8ea9a6e119bc7c3c609a6cb55b37e6e2
andreaskadhede <andreaskadhede@icloud.com>
2024-10-17 13:59:57 +0200

    pakkeliste + category igen

--- src/pages/Pakkeliste.jsx
+++ src/pages/Pakkeliste.jsx
@@ -1,26 +1,30 @@
+import { useState } from "react";
 import Category from "../components/Category";
 
 export default function Pakkeliste() {
+   const [totalCategoryItems, setTotalCategoryItems] = useState(0);
+   const [totalCheckedCategoryItems, setTotalCheckedCategoryItems] = useState(0);
+
    return (
-    <main>
-      <div className="packinglist">
-         <div className="packinglist-fixed">
-         <div className="packinglist-header">
-            <h1>Riga</h1>
-            <h1>17/20</h1>
+      <main>
+         <div className="packinglist">
+            <div className="packinglist-fixed">
+               <div className="packinglist-header">
+                  <h1>Riga</h1>
+                  <h1>{`${totalCheckedCategoryItems}/${totalCategoryItems}`}</h1>
+               </div>
+               <div className="packinglist-info">
+                  <h4>16.-23. maj 2024</h4>
+                  <button>+ tilføj kategori</button>
+               </div>
+               <div className="linje-moenster"></div>
+            </div>
+            {/* Pass the totalCategoryItems and setter to each Category component */}
+            <Category totalCategoryItems={totalCategoryItems} setTotalCategoryItems={setTotalCategoryItems} totalCheckedCategoryItems={totalCheckedCategoryItems} setTotalCheckedCategoryItems={setTotalCheckedCategoryItems}/>
+            <Category totalCategoryItems={totalCategoryItems} setTotalCategoryItems={setTotalCategoryItems} totalCheckedCategoryItems={totalCheckedCategoryItems} setTotalCheckedCategoryItems={setTotalCheckedCategoryItems}/>
+            {/* If you need another Category, you can add it like this: */}
+            {/* <Category totalCategoryItems={totalCategoryItems} setTotalCategoryItems={setTotalCategoryItems} /> */}
          </div>
-         <div className="packinglist-info">
-            <h4>16.-23. maj 2024</h4>
-            <button>+ tilføj kategori</button>
-         </div>
-         <div className="linje-moenster"></div>
-         </div>
-         <Category />
-         <Category />
-         <Category />
-         <Category />
-         <Category />
-      </div>
       </main>
    );
 }
