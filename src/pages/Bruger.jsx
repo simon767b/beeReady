34b11e499f24ce53a390a433549e1cb4a9ee65cf
Inger-Margrethe <imhp@hotmail.dk>
2024-10-22 12:47:36 +0200

    bruger started

--- src/pages/Bruger.jsx
+++ src/pages/Bruger.jsx
@@ -1,11 +1,31 @@
-import AddElementHex from "../components/AddElementHex";
-
 export default function Bruger() {
   return (
     <>
       <main className="bruger">
-        <AddElementHex />
+        <div className="hex_profile_img">
+          <img
+            src="img\dummy_profile_img.jpg"
+            alt="Midlertidigt profilbillede"
+          />
+        </div>
+        <section className="user_info">
+          <article>
+            <label>
+              Navn:
+              <input type="text" value={"Patricia"} readOnly />
+            </label>
+            <button>Skift</button>
+          </article>
+          <article>
+            <label>
+              Navn:
+              <input type="text" readOnly />
+            </label>
+            <button>Skift</button>
+          </article>
+        </section>
       </main>
+      <img className="hex_bg_pattern" src="img\hex_bg_pattern.svg" alt="Bikubemønster baggrund" />
     </>
   );
 }
