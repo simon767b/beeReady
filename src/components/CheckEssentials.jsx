038676435d79cef15131c964110487cdd03c6a45
andreaskadhede <andreaskadhede@icloud.com>
2024-10-22 12:45:37 +0200

    essentials

--- src/components/CheckEssentials.jsx
+++ src/components/CheckEssentials.jsx
@@ -1,8 +1,14 @@
-export default function Check() {
+export default function Check({ checked, onChange }) {
   return (
-    <div className="hex_check_essentials outer">
-      <div className="hex_check_essentials inner">
+    <label className="hex_check outer">
+      <div className="hex_check inner">
+        <input type="checkbox" checked={checked} onChange={onChange} />
+        <img
+          className="tick_mark"
+          src="img\icons\tick_mark.svg"
+          alt="Flueben"
+        />
       </div>
-    </div>
+    </label>
   );
 }
