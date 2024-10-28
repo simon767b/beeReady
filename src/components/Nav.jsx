8babaf98c4b70b376515b2c7bedd3ab9002c783a
andreaskadhede <andreaskadhede@icloud.com>
2024-10-28 14:43:24 +0100

    test af ny billedsti

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,10 +1,11 @@
 import { Link } from "react-router-dom";
+import bruger from "/img/icons/bruger.svg";
 
 export default function Nav() {
    return (
       <div className="navbar">
          <Link to="/bruger" className="navbar_navitems navLinks">
-            <img src="/img/icons/bruger.svg" alt="nav ikon" />
+            <img src={bruger} alt="nav ikon" />
             <p>Bruger</p>
          </Link>
          <Link to="/" className="navbar_navitems navLinks">
