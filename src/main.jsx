c9c5af2e87ab96ca765fd8128a86d73435774299
andreaskadhede <andreaskadhede@icloud.com>
2024-10-28 11:35:10 +0100

    efter github crash hos Andrea

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import { BrowserRouter } from 'react-router-dom';
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { BrowserRouter } from "react-router-dom";
 
 createRoot(document.getElementById("root")).render(
-  <StrictMode>
-    <BrowserRouter basename={import.meta.env.DEV ? "/" : "/beeReady/"}>
-      <App />
-    </BrowserRouter>
-  </StrictMode>
+   <StrictMode>
+      <BrowserRouter basename={import.meta.env.DEV ? "/" : "/beeReady/"}>
+         <App />
+      </BrowserRouter>
+   </StrictMode>
 );
